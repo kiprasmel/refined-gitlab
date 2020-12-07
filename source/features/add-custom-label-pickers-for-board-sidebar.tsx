kiprasmel/refined-gitlab

commit ec9413f7a56fde39e0550b79d1a2071ab52ae8f7

refactor: re-use `observedValueFactory` to avoid having the same hook

source/features/add-custom-label-pickers-for-board-sidebar.tsx
@@ -1,10 +1,34 @@
 import React, { FC } from "react";
 
 import select from "select-dom";
-import { useObservedIssueIid } from "../hooks/useObservedIssueIid";
+import { observedValueFactory } from "../hooks/useObservedValue";
 import { Feature, features } from "../Features";
 import { CustomLabelPicker } from "../components/CustomLabelPicker";
 import { renderBefore } from "../utils/render";
+
+const useObservedIssueIid = observedValueFactory((mutationRecords) => {
+	const mutation = mutationRecords[0];
+
+	if (mutation.type !== "characterData") {
+		console.info("ignoring (1) sidebar issue id update: type didn't match `characterData`");
+		return;
+	}
+
+	if (mutation.target.nodeName !== "#text") {
+		console.info("ignoring (2) sidebar issue id update: target.nodeNam didn't match `#text`");
+		return;
+	}
+
+	const value = mutation.target.nodeValue?.trim().replace("#", "");
+
+	if (value === undefined) {
+		console.info("ignoring (3): target.nodeValue === undefined");
+		return;
+	}
+
+	const parsedVal: number = parseInt(value, 10);
+	return parsedVal;
+});
 
 export const addCustomLabelPickersForBoardSidebar: Feature = ({ sidebarFeaturesFromLabels }) => {
 	if (!/\/boards?\/?\d*/.test(window.location.href)) {

source/hooks/useObservedIssueIid.tsx
@@ -1,49 +1,0 @@
-import { useState } from "react";
-import select from "select-dom";
-
-/**
- * only available for the `board`'s sidebar, since the issue's ID is displayed there.
- * Otherwise it's usually available from the URL
- */
-export const useObservedIssueIid = (): number | undefined => {
-	const [issueIid, setIssueIid] = useState<number | undefined | typeof NaN>(undefined);
-
-	const observer = new MutationObserver((mutationRecords) => {
-		const mutation = mutationRecords[0];
-
-		if (mutation.type !== "characterData") {
-			console.info("ignoring (1) sidebar issue id update: type didn't match `characterData`");
-			return;
-		}
-
-		if (mutation.target.nodeName !== "#text") {
-			console.info("ignoring (2) sidebar issue id update: target.nodeNam didn't match `#text`");
-			return;
-		}
-
-		const value = mutation.target.nodeValue?.trim().replace("#", "");
-
-		if (value === undefined) {
-			console.info("ignoring (3): target.nodeValue === undefined");
-			return;
-		}
-
-		const parsedVal: number = parseInt(value, 10);
-
-		setIssueIid(parsedVal);
-	});
-
-	const issueIidElement: HTMLSpanElement | null = select(".issuable-header-text > span");
-
-	if (!issueIidElement) {
-		throw new Error("Cannot render label pickers in issue board - the `issueIidElement` was falsy");
-	}
-
-	observer.observe(issueIidElement, {
-		childList: true, //
-		subtree: true,
-		characterData: true,
-	});
-
-	return issueIid;
-};

commit 90b13d934cf534941a11397cb261be722c4c7d58

fix: pass in the `issueIidElement` to observer
the regression was introduced in ec9413f7a56fde39e0550b79d1a2071ab52ae8f7

how'd it even work lmao

Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

source/features/add-custom-label-pickers-for-board-sidebar.tsx
@@ -54,6 +54,12 @@ export const addCustomLabelPickersForBoardSidebar: Feature = ({ sidebarFeaturesF
 		throw new Error("Cannot render label pickers - the `$('.labels')[0].nextElementSibling` was falsy");
 	}
 
+	const issueIidElement: HTMLSpanElement | null = select(".issuable-header-text > span");
+
+	if (!issueIidElement) {
+		throw new Error("[refined-gitlab] issue iid element not found -- cannot parse issue :/");
+	}
+
 	for (const {
 		title = "", //
 		labels = [],
@@ -66,7 +72,7 @@ export const addCustomLabelPickersForBoardSidebar: Feature = ({ sidebarFeaturesF
 		}
 
 		const ReactiveCustomLabelPicker: FC = () => {
-			const issueIid = useObservedIssueIid();
+			const issueIid = useObservedIssueIid(issueIidElement);
 
 			if (!issueIid && issueIid !== 0) {
 				return null;

source/hooks/useObservedValue.ts
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import select from "select-dom";
 
 export const observedValueFactory = <T>(parseValueOnElementChange: (mutationRecords: MutationRecord[]) => T) =>
-	function useObservedValue(elementToObserve: HTMLElement | null = select(`ul[data-board]`)): T | undefined {
+	function useObservedValue(elementToObserve: HTMLElement): T | undefined {
 		if (!elementToObserve) {
-			throw new Error("Cannot render label pickers in issue board - the `elementToObserve` was falsy");
+			const err = new Error(
+				"[refined-gitlab] Cannot render element in issue board - the `elementToObserve` was falsy"
+			);
+			console.error(err, elementToObserve);
 		}
 
 		const [observedValue, setObservedValue] = useState<T | undefined>(undefined);